3fb3068e95b53f6cbf843ffce60c6d2b9d2bf23c
ishfaqkhan-dev <ishfaqkhan.dev@gmail.com>
2026-06-23 06:25:23 -0800

    Done

---
+++ Dart_Programming_Tutorial_Practice_files/Dart_Tutorial_02_Practice.dart
@@ -0,0 +1,26 @@
+void main() {
+  // var marks = 30;
+  // String name = "Muhammad Ishfaq Khan";
+  // int x = 10;
+  // double y = 15.05;
+
+  // dynamic e = 10;
+  // e = "ishfaq";
+
+  int w = 10;
+  print(w);
+
+  double y = 20.05;
+  print(y);
+
+  // string datatype
+
+  String course = "Flutter Development";
+  print(course);
+
+  final String name = "Flutter Developers";
+  print("Hello, $name");
+
+  const x = 10;
+  print(x);
+}
