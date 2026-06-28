1496acd599eb169e6adce5f957100c71f89921c4
ishfaqkhan-dev <ishfaqkhan.dev@gmail.com>
2026-06-27 20:25:44 -0800

    Done

---
+++ Dart_Programming_Tutorial_Practice_files/Dart_Tutorial_03_Practice.dart
@@ -0,0 +1,52 @@
+void main() {
+  // == > Arithmatic Operator < ==
+
+  // int a = 20;
+  // int b = 3;
+
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(a / b);
+  // print(a ~/ b);
+  // print(a % b);
+
+  // == > Relational Operator < ==
+
+  // int x = 5;
+  // int y = 3;
+
+  // print(x == y);
+  // print(x != y);
+  // print(x >= y);
+  // print(x <= y);
+  // print(x > y);
+  // print(x < y);
+
+  // == > Logical Operators < ===
+
+  // int a = 10;
+  // int b = 20;
+  // int c = 15;
+
+  // print((a <= b) && (a < c));
+  // print((a <= b) || (a > c));
+  // print(!(a < b));
+
+  // == > Assignment OPerator < ==
+
+  // int x = 5;
+  // x += 2;
+  // x = x + 2;
+
+  // == > Incre and decre OPerator < ==
+
+  // int count = 4;
+  // count++; // count = count + 1
+  // print(count);
+
+  // == >Null Aware OPerator < ==
+  // String? username;
+  // username = "Ishfaq";
+  // print(username ?? 'Guest');
+}
