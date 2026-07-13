7f7e8a11474079d04ac14c0c0de17308f00cfae6
ishfaqkhan-dev <ishfaqkhan.dev@gmail.com>
2026-07-12 17:45:34 -0800

    Done

---
+++ Dart_Programming_Tutorial_Practice_files/Dart_Tutorial_05_Practice.dart
@@ -0,0 +1,48 @@
+void main() {
+  // ==== > For loop < ====
+
+  // for (int i = 1; i <= 10; i++) {
+  //   // print("Hello, World");
+  //   print(i);
+  // }
+
+  // print("Loop is over");
+
+  // ==== > While Loop< ====
+
+  // int count = 1;
+
+  // while (count <= 5) {
+  //   print(count);
+  //   count++;
+  // }
+
+  // ==== >do While Loop< ====
+
+  // int count = 6;
+
+  // do {
+  //   print(count);
+  //   count++;
+  // } while (count <= 5);
+
+  // print("Loop is over");
+
+  // ==== > for-in loop < =====
+
+  // List<String> fruits = ['Apple', 'Banana', 'Mango'];
+  // print(fruits[1]);
+  // print(fruits);
+  // for (String fruit in fruits) {
+  //   print(fruit);
+  // }
+
+  // // ==== > break and continue statment < ====
+
+  // for (int i = 1; i <= 10; i++) {
+  //   if (i == 5) {
+  //     continue;
+  //   }
+  //   print(i);
+  // }
+}
