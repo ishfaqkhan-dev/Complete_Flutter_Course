e3e8189ca24aa3a167957cd2af6a7c3cd8d7eed7
ishfaqkhan-dev <ishfaqkhan.dev@gmail.com>
2026-07-22 21:27:18 -0800

    Done

---
+++ Dart_Programming_Tutorial_Practice_files/Dart_Tutorial_06_Practice.dart
@@ -0,0 +1,49 @@
+void main() {
+  // String name = 'Ishfaq Khan';
+  // print(name);
+
+  // String var1 = 'Code';
+  // String var2 = 'With Ishfaq';
+
+  // String var3 = var1 + " " + var2;
+
+  // print(var1);
+  // print(var2);
+  // print("----------------");
+  // print(var3);
+
+  // String name = "Muhammad Ishfaq Khan";
+  // int age = 21;
+
+  // // print("My name is " + name + " and my age is " + age); // wrong
+  // print("My name is $name and my age is $age. and after one year my age will be ${age + 1}");
+
+  // String multi_line_string = """this is the line 1.
+  // and this is the line 2.
+  // jkajkajsdkasda           haishdjashdjahd  jaisddddddddd
+  // ahsdahkdhakhdakdh
+  // ajskdjaksdjk""";
+
+  // print(multi_line_string);
+
+  // String name = "Muhammad Ishfaq Khan";
+
+  // print(name);
+  // print(name.toUpperCase());
+  // print(name.toLowerCase());
+  // print(name.trim());
+  // print(name.length);
+  // print(name.contains("kh"));
+  // print(name.replaceAll("Ishfaq", "Hammad"));
+
+// String sentence = 'Dart+is+fun+to+learn';
+// List<String> words = sentence.split('+');
+
+// print(words);
+
+// String text = '';
+
+// print(text.isEmpty);
+// print(text.isNotEmpty);
+
+}
