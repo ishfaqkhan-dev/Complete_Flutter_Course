f656e42d45bb88c76c34b33593812eaa52173fcc
ishfaqkhan-dev <ishfaqkhan.dev@gmail.com>
2026-07-25 18:16:24 -0800

    Done

---
+++ Dart_Programming_Tutorial_Practice_files/Dart_Tutorial_07_Practice.dart
@@ -0,0 +1,35 @@
+void main() {
+  List<String> fruits = ['Apple', 'Banana', 'Mango'];
+
+  // for (String fruit in fruits) {
+  //   print(fruit);
+  // }
+
+  // print(fruits);
+
+  // print(fruits[0]);
+  // print(fruits[1]);
+
+  // print(fruits.first);
+  // print(fruits.last);
+
+  // fruits.add('Orange');
+  // print(fruits);
+
+  // fruits.remove('Banana');
+  // print(fruits);
+
+  // fruits[1] = 'Grapes';
+  // print(fruits);
+
+  List<int> numbers = [5, 3, 8, 1];
+  // numbers.sort();
+  // numbers.reversed;
+  // numbers.reversed;
+  // print(numbers.reversed);
+
+  // print(numbers.indexOf(3));
+  print(numbers);
+  numbers.clear();
+  print(numbers);
+}
